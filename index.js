2d79fe97f2474f9b9893712a3a4ddf91fa2edae5
thomas <thomascoles14@gmail.com>
2021-03-13 00:30:08 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+ function add(a,b) {
+    return a+b
+    var Mynumber = 40
+    console.log(Mynumber + 1);
+    console.log(1 + Mynumber)
+ }
+ function subtract(b,a) {
+    return b-a
+ }
+ function  multiply(a,b) {
+     return a*b
+ }
+ function divide(a,b) {
+     return a/b
+ }
+ makeInt(string)
+ return parseInt('2', 10);
+ 
+    
